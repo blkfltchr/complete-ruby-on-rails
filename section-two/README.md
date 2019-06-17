283c055fb24e6990f2c434e234be05793ae9d8f0
Blake Fletcher <blkfltchr@gmail.com>
2019-06-16 21:56:12 -0400

    Initial commit - hello world

---
+++ section-two/README.md
@@ -0,0 +1,6 @@
+# Complete Ruby on Rails Developer Course
+
+Course notes, applications and exercies from [Complete Ruby on Rails Developer Course](https://www.udemy.com/the-complete-ruby-on-rails-developer-course/) by Rob Percival & Mashrur Hossain.
+
+Contents:
+- [x] `/section-two` - Fundamentals of the Ruby programming language
---
+++ section-two/helloworld.rb
@@ -0,0 +1,27 @@
+# Three ways to ouput Hello world
+
+print "Hello world 1\n" 
+
+# outputs Hello world (needs new line)
+
+puts "Hello world 2"
+
+# outputs Hello world
+
+p "Hello world 3"
+
+# outputs "Hello world" (with quotation marks)
+
+# With a variable
+
+greeting = "Hello world 4"
+
+puts greeting
+
+# With a method
+
+def say_hello(greeting)
+    puts greeting
+end
+
+say_hello "Hello world 5" # call it like so
